b6d432ccd2bc5e62917b7089a98b3563ae86cbc5
Manuel Spitschan <mspits@sas.upenn.edu>
2016-04-19 19:47:25 -0400

    A few changes
    Signed-off-by: Manuel Spitschan <mspits@sas.upenn.edu>

--- AnalysisProtocols/PupilAnalysisToolbox_MaxMelPilot3Sec.m
+++ AnalysisProtocols/PupilAnalysisToolbox_MaxMelPilot3Sec.m
@@ -25,6 +25,12 @@ Data = PupilAnalysisToolbox_PulseSequentialTrialAnalysis(params, Subjects, Proto
 %PupilAnalysisToolbox_PlotTimeSeriesAndMean(Data, Subjects, resultsPath, params.xLim, params.yLim);
 %%
 Protocols={'MaxMelLMSPilot_Mel'};
+params.StepDurSecs = 3;
+params.yLim = 0.8;
+params.TrialInspectorFlag = false; % trial-by-trial turned off
+params.PulseOnset = 4.5;
+params.meanCenterWindow = [0 params.PulseOnset-1/params.sampling_frequency]; % In seconds
+params.valid_trial_length = 35;
 
 basePath = ['/Users/' userID '/Dropbox (Aguirre-Brainard Lab)/MELA_data/MaxMelLMSPilot_Mel'];
 resultsPath = ['/Users/' userID '/Dropbox (Aguirre-Brainard Lab)/MELA_analysis/MaxMelLMSPilot'];
@@ -32,4 +38,4 @@ resultsPath = ['/Users/' userID '/Dropbox (Aguirre-Brainard Lab)/MELA_analysis/M
 oldLabels = {'MaxMel400Pct-45sBackground', 'MaxMel400Pct-45sPositivePulse3s'};
 newLabels = {'MelBackground', 'Mel400Pct'};
 Data = PupilAnalysisToolbox_PulseSequentialTrialAnalysis(params, Subjects, Protocols, newLabels, oldLabels, basePath, resultsPath);
-PupilAnalysisToolbox_PlotTimeSeriesAndMean(Data, Subjects, resultsPath, params.xLim, params.yLim);
+%PupilAnalysisToolbox_PlotTimeSeriesAndMean(Data, Subjects, resultsPath, params.xLim, params.yLim);
