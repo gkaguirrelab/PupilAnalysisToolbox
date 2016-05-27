750f3108933023b58786e6a32242ed8ae29840af
Manuel Spitschan <mspits@sas.upenn.edu>
2016-05-27 15:32:36 -0400

    Added
    Signed-off-by: Manuel Spitschan <mspits@sas.upenn.edu>

--- AnalysisProtocols/PupilAnalysisToolbox_MaxMelPilot3Sec.m
+++ AnalysisProtocols/PupilAnalysisToolbox_MaxMelPilot3Sec.m
@@ -11,11 +11,7 @@ params.meanCenterWindow = [0 params.PulseOnsetSecs-1/params.sampling_frequency];
 params.TrialInspectorFlag = false;
 
 Protocols={'MelanopsinMRPupil_MaxLMS400Pct3sPulse'};
-<<<<<<< Updated upstream
-Subjects = {'HERO_mxs1'};
-=======
-Subjects = {'HERO_aso1'};
->>>>>>> Stashed changes
+Subjects = {'HERO_asb1' 'HERO_aso1' 'HERO_gka1' 'HERO_mxs1'};
 
 basePath = ['/Users/' userID '/Dropbox (Aguirre-Brainard Lab)/MELA_data/MelanopsinMRPupil_MaxLMS400Pct3sPulse'];
 resultsPath = ['/Users/' userID '/Dropbox (Aguirre-Brainard Lab)/MELA_analysis/MelanopsinMRPupil_MaxLMS400Pct3sPulse'];
@@ -25,8 +21,8 @@ newLabels = {'Background' 'LMS400Pct'}
 oldLabels = {'MaxLMS400Pct-45sBackground', 'MaxLMS400Pct-45sPositivePulse3s'};
 Data = PupilAnalysisToolbox_PulseSequentialTrialAnalysis(params, Subjects, Protocols, newLabels, oldLabels, basePath, resultsPath);
 
-%PupilAnalysisToolbox_PlotTimeSeriesAndMean(Data, Subjects, resultsPath, params, {[2 3]}, {[1 -1]});
-%PupilAnalysisToolbox_SummarizeDataQuality(Subjects, resultsPath);
+PupilAnalysisToolbox_PlotTimeSeriesAndMean(Data, Subjects, resultsPath, params, [], []);
+PupilAnalysisToolbox_SummarizeDataQuality(Subjects, resultsPath);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% SilentSubstitutionPIPR_PIPR5_5sPulse
@@ -38,11 +34,7 @@ params.meanCenterWindow = [0 params.PulseOnsetSecs-1/params.sampling_frequency];
 params.TrialInspectorFlag = false;
 
 Protocols={'MelanopsinMRPupil_MaxMel400Pct3sPulse'};
-<<<<<<< Updated upstream
-Subjects = {'HERO_mxs1'};
-=======
-Subjects = {'HERO_aso1'};
->>>>>>> Stashed changes
+Subjects = {'HERO_asb1' 'HERO_aso1' 'HERO_gka1' 'HERO_mxs1'};
 
 basePath = ['/Users/' userID '/Dropbox (Aguirre-Brainard Lab)/MELA_data/MelanopsinMRPupil_MaxMel400Pct3sPulse'];
 resultsPath = ['/Users/' userID '/Dropbox (Aguirre-Brainard Lab)/MELA_analysis/MelanopsinMRPupil_MaxMel400Pct3sPulse'];
@@ -52,5 +44,4 @@ newLabels = {'Background' 'Mel400Pct'}
 oldLabels = {'MaxMel400Pct-45sBackground', 'MaxMel400Pct-45sPositivePulse3s'};
 Data = PupilAnalysisToolbox_PulseSequentialTrialAnalysis(params, Subjects, Protocols, newLabels, oldLabels, basePath, resultsPath);
 
-%PupilAnalysisToolbox_PlotTimeSeriesAndMean(Data, Subjects, resultsPath, params, {[2 3]}, {[1 -1]});
-%PupilAnalysisToolbox_SummarizeDataQuality(Subjects, resultsPath);
+PupilAnalysisToolbox_PlotTimeSeriesAndMean(Data, Subjects, resultsPath, params, [], []);
