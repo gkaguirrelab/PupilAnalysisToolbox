afae50fd89aea6f28e73a13831f75d61e100e927
Manuel Spitschan <mspits@sas.upenn.edu>
2016-06-10 12:38:22 -0400

    Added protocol
    Signed-off-by: Manuel Spitschan <mspits@sas.upenn.edu>

---
+++ AnalysisProtocols/PupilAnalysisToolbox_MelanopsinPupilMaxLMS_3sPulse17sSegment.m
@@ -0,0 +1,47 @@
+[~, userID] = system('whoami');
+userID = strtrim(userID);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% MelanopsinPupilMaxLMS_3sPulse17sSegment
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+params = PupilAnalysisToolbox_GetDefaultParams;
+params.BadNaNThreshold = 0.5;
+params.PulseOnsetSecs = 1;
+params.PulseDurationSecs = 3;
+params.meanCenterWindow = [0 1-1/params.sampling_frequency]; % In seconds
+params.TrialInspectorFlag = false;
+params.full_trial_length = 17;
+params.final_trial_length = 14;
+params.minimum_trial_length = 14;
+params.xLim = 14;
+Protocols={'MelanopsinPupilMaxLMS_3sPulse17sSegment'};
+Subjects = {'JOB1'};
+Dates = {'060816'};
+basePath = ['/Users/' userID '/Dropbox (Aguirre-Brainard Lab)/MELA_data/MelanopsinPupilMaxLMS_3sPulse17sSegment'];
+resultsPath = ['/Users/' userID '/Dropbox (Aguirre-Brainard Lab)/MELA_analysis/MelanopsinPupilMaxLMS_3sPulse17sSegment'];
+newLabels = {'LMS400Pct'}
+oldLabels = {'MelanopsinPupilMaxLMS-PulseMaxLMS_3s_MaxContrast17sSegment'};
+Data = PupilAnalysisToolbox_PulseSequentialTrialAnalysis(params, Subjects, Dates, Protocols, newLabels, oldLabels, basePath, resultsPath);
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% MelanopsinPupilMaxLMS_3sPulse17sSegment
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+params = PupilAnalysisToolbox_GetDefaultParams;
+params.BadNaNThreshold = 0.5;
+params.PulseOnsetSecs = 1;
+params.PulseDurationSecs = 3;
+params.meanCenterWindow = [0 1-1/params.sampling_frequency]; % In seconds
+params.TrialInspectorFlag = false;
+params.full_trial_length = 17;
+params.final_trial_length = 14;
+params.minimum_trial_length = 14;
+params.xLim = 14;
+Protocols={'MelanopsinPupilMaxMel_3sPulse17sSegment'};
+Subjects = {'JOB1'};
+Dates = {'060816'};
+basePath = ['/Users/' userID '/Dropbox (Aguirre-Brainard Lab)/MELA_data/MelanopsinPupilMaxMel_3sPulse17sSegment'];
+resultsPath = ['/Users/' userID '/Dropbox (Aguirre-Brainard Lab)/MELA_analysis/MelanopsinPupilMaxMel_3sPulse17sSegment'];
+newLabels = {'Mel400Pct'}
+oldLabels = {'MelanopsinPupilMaxMel-PulseMaxMel_3s_MaxContrast17sSegment'};
+Data = PupilAnalysisToolbox_PulseSequentialTrialAnalysis(params, Subjects, Dates, Protocols, newLabels, oldLabels, basePath, resultsPath);
